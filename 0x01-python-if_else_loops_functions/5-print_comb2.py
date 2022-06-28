3850a59136e08061a8bcda602893bec1701c1f22
Joshua Alonge <alongejoshua@gmail.com>
2022-06-28 13:38:45 -0700

    prints 00 to 99 (updated)

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i == 99:
+    if (i == 99):
         print("{}".format(i))
+        break;
     print("{:d}{:0.0f}".format(int(i / 10), i % 10), end=", ")
