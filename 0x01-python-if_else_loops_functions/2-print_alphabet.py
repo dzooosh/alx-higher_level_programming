d9d40c57bba041dcc2de867108b35cb43dd7a592
Joshua Alonge <alongejoshua@gmail.com>
2022-06-28 12:49:38 -0700

    update on programs

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # prints ASCII alphabet in lowercase
 for i in range(97, 123):
-    print(f"{chr(i)}",end="")
+    print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for alpha in range(97, 123):
     if (chr(alpha) == "q" or chr(alpha) == "e"):
         continue
     else:
-        print(f"{chr(alpha)}",end="")
+        print("{}".format(chr(alpha)), end="")
