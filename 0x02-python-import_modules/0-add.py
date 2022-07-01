eff7f55f918bc15695c49cceb11814d5a3f35c94
Joshua Alonge <alongejoshua@gmail.com>
2022-06-30 21:17:34 -0700

    imports a function and prints result

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import add_0 as ad
 
-a = 1
-b = 2
+if __name__ = "__main__":
+    import add_0 as ad
 
-print("{} + {} = {}".format(a, b, ad.add(a, b)))
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, ad.add(a, b)))
