e8738661e850f47cf296d0112250fede2d655945
Joshua Alonge <alongejoshua@gmail.com>
2022-07-02 06:39:12 -0700

    retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """retrieves an element index idx from my_list"""
+    if (idx < 0):
+        return None
+    if (idx >= len(my_list)):
+        return None
+    return my_list[idx]
