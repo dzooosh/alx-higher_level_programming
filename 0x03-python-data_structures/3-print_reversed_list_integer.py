0e606dfdff7d3b6835d6392376d0e13282d86acb
Joshua Alonge <alongejoshua@gmail.com>
2022-07-02 07:30:34 -0700

    update on code using for instead of while

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    """ prints all integer of a list in reversed order """
+    """ prints all integer of a list in reversed order
     i = len(my_list) - 1
     while (i >= 0):
+       print("{:d}".format(my_list[i]))
+       i -= 1
+"""
+    for i in range((len(my_list) - 1), -1, -1):
         print("{:d}".format(my_list[i]))
-        i -= 1
+
+print_reversed_list_integer([1,2,3,4,5])
