7ed830672f82f3d7ace813e8e0ebb38c96c5157f
Joshua Alonge <alongejoshua@gmail.com>
2022-07-06 16:50:05 -0700

    returns key with the highest value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# function that returns the key with the highest integer value
+def best_score(a_dictionary):
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
