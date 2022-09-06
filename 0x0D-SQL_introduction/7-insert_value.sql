fe001cad73f191c564bde517aad5f9b01f5c2528
Joshua Alonge <alongejoshua@gmail.com>
2022-09-06 15:36:25 -0700

    update task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 -- inserts a new row in the tale first_table from db in your mySQL server
-USE hbtn_0c_0;
-INSERT INTO first_table (id, name)
+INSERT INTO `first_table` (id, name)
 VALUES (89, 'Best School');
