fd507e9628b992685f3ca54a8e9ce008aadf69d1
Joshua Alonge <alongejoshua@gmail.com>
2022-09-06 15:40:05 -0700

    update to task10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,6 +2,5 @@
 -- of the database hbtn_0c_0 in mysql server
 -- results should display both score and the name (in this order)
 -- records should be ordered by score (top first)
-USE hbtn_0c_0;
 SELECT score, name FROM second_table 
 ORDER BY score DESC;
