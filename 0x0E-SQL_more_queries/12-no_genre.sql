1f8fc748bb7d7b9451dadb6af9c7904fc0728fbb
Joshua Alonge <alongejoshua@gmail.com>
2022-09-08 15:47:50 -0700

    lists all shows in db without a genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- lists all shows contained in a db with at least one linked
+SELECT s.title, g.genre_id FROM tv_shows s 
+LEFT JOIN tv_show_genres g 
+ON s.id = g.show_id 
+WHERE g.show_id = NULL
+ORDER BY 
+s.title ASC,
+g.genre_id ASC;
