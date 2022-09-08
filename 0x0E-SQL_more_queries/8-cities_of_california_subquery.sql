04e1de5001df6746e8d129aecc127552e000d761
Joshua Alonge <alongejoshua@gmail.com>
2022-09-08 07:33:07 -0700

    lists all the cities of California using two tables without JOIN

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- lists all the cities of California found in the db hbtn_0d_usa
-SELECT cities.id, states.name FROM cities C, states S
-WHERE C.id = S.id
-WHERE name = California
-ORDER BY C.id;
+SELECT C.id, C.name FROM cities C, states S
+WHERE C.state_id = S.id AND S.name = 'California'
+ORDER BY C.id ASC;
