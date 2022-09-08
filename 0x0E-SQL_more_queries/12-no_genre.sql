1c8d5dba2f6c42536ac6b2b899ebfdcefe3dfab8
Joshua Alonge <alongejoshua@gmail.com>
2022-09-08 16:02:18 -0700

    update code

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 SELECT s.title, g.genre_id FROM tv_shows s 
 LEFT JOIN tv_show_genres g 
 ON s.id = g.show_id 
-WHERE g.show_id = NULL
+WHERE g.show_id IS NULL
 ORDER BY 
 s.title ASC,
 g.genre_id ASC;
