d66bf8b83dcbcdb290072473843049d8b6a6624c
Joshua Alonge <alongejoshua@gmail.com>
2022-10-03 15:10:36 -0700

    prints second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,17 +2,18 @@
 
 const process = require('process');
 const args = process.argv;
-let min = 0;
-let str = [];
+const str = [];
 
 if (!args[2]) {
-  console.log("0");
+  console.log('0');
 } else if (args.length === 3) {
-    console.log("0");
+  console.log('0');
 } else {
-    args.forEach((arg) => {
-	   str.push(arg);
-    });
-    str.sort(function(a, b) { return a-b;	});
-    console.log(str[str.length-2]);
+  args.forEach((arg) => {
+    str.push(arg);
+  });
+  str.sort(function (a, b) {
+    return (a - b);
+  });
+  console.log(str[str.length - 2]);
 }
