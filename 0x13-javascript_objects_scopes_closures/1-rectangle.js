cff7193c0d87b133466fae563969ba804d5ecf1f
Joshua Alonge <alongejoshua@gmail.com>
2022-10-04 08:41:32 -0700

    initializing the instance attributes with constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  // class Rectangle
+  constructor (w, h) {
+    // initializing the width and height attribute
+    this.width = w;
+    this.height = h;
+  }
+};
