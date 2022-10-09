e3320b9ea4e6f9ee58d51393f3a91b169e15d5d3
Joshua Alonge <alongejoshua@gmail.com>
2022-10-09 11:37:58 -0700

    lists state name based on argument and prevents sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Displays all values in the states table of hbtn_0e_0_usa
+    based on state name searched
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    data = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (data,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
