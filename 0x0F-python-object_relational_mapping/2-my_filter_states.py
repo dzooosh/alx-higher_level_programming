b63934f25c473de4f4b09e8191e4c2db5d18049c
Joshua Alonge <alongejoshua@gmail.com>
2022-10-09 10:48:40 -0700

    lists state names based on arguments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,7 +2,6 @@
 """
     Displays all values in the states table of hbtn_0e_0_usa
     based on state name searched
-    
 """
 import MySQLdb
 import sys
@@ -12,7 +11,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     data = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(data))
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE BINARY '{}'".format(data))
     rows = cur.fetchall()
     for row in rows:
         print(row)
