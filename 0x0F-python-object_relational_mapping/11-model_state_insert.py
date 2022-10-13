c7b09f89a684d0c6df2d8a306d9bb12489d882d5
Joshua Alonge <alongejoshua@gmail.com>
2022-10-13 09:16:19 -0700

    adds a new object Louisiana to the database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" adds a new object to the database """
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(session.query(State.id).filter(State.name == "Louisiana").first()[0])
