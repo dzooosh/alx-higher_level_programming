f2aea52532fd69ffb5acbbecaccea3d4e7bda68a
Alonge Joshua <alongejoshua@gmail.com>
2022-10-27 09:47:31 -0700

    takes a URL, sends a GET request and displays the body of the res

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an arg, sends a GET request with a custom header and displays the body of the res
-curl -H "X-School-User-Id: 98" $1 -X GET
+curl -H "X-School-User-Id: 98" $1
