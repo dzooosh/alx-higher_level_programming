53aaf9694e15f19225f65ad5bea47b7eacc51093
Joshua Alonge <alongejoshua@gmail.com>
2022-10-31 05:12:08 -0700

    fetches a site using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" fetches alx-intranet.hbtn.io/status site using
-urllib request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        data = res.read()
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- ut8 content: {}".format(data.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
