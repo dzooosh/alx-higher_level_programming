006cd0a1935719ec80b8ae7554fde0b6b48b9a40
Joshua Alonge <alongejoshua@gmail.com>
2022-11-27 13:49:45 +0100

    prints the number of movies where Wedge Antilles is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 // prints the no of movies
-//+ where "Wedge Antilles" is present
+// + where "Wedge Antilles" is present
 
 const process = require('process');
 const request = require('request');
 const url = process.argv[2];
-const person_id = "https://swapi-api.hbtn.io/api/people/18/"
+const person_id = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 request(url, (error, response, body) => {
   if (error) console.log(error);
   const obj = JSON.parse(body);
-  //console.log(obj.results[0]["characters"]);
-  for (let result of obj.results) {
-    if (result["characters"].includes(person_id)) {
-      count+=1;
+  for (const result of obj.results) {
+    if (result.characters.includes(person_id)) {
+      count += 1;
     }
   }
   console.log(count);
